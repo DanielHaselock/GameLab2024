e7b1aa1d370a864314aa19f6ed247079530806ce
5DollarCostcoChicken <kiran.freund8@gmail.com>
2024-02-23 17:02:53 -0500

    Added basic AI enemies and Broke Physics (whoops)

---
+++ Assets/Scripts/Enemies/EnemyHider.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+public class EnemyHider : Enemy
+{
+    public override void FixedUpdateNetwork()
+    {
+        base.FixedUpdateNetwork();
+        if (_targetPlayer != null && Vector3.Distance(transform.position, _targetPlayer.transform.position) < 10)
+        {
+            transform.position -= (_targetPlayer.transform.position - transform.position).normalized * moveSpeed * Runner.DeltaTime;
+        }
+        if (_seenPlayers.Count > 1)
+            ChangeTargeting();
+    }
+    public override void ChangeTargeting()
+    {
+        base.ChangeTargeting();
+        switch (_seenPlayers.Count)
+        {
+            case 0:
+                _targetPlayer = null;
+                break;
+            case 1:
+                _targetPlayer = _seenPlayers[0];
+                break;
+            default:
+                if (Vector3.Distance(transform.position, _seenPlayers[0].transform.position) < Vector3.Distance(transform.position, _seenPlayers[1].transform.position))
+                    _targetPlayer = _seenPlayers[1];
+                else
+                    _targetPlayer = _seenPlayers[0];
+                break;
+        }
+    }
+}
