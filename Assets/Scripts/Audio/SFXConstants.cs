1032e18229dacd228874185549a9c1ffb38d50e6
5DollarCostcoChicken <kiran.freund8@gmail.com>
2024-04-16 12:35:33 -0400

    .

--- Assets/Scripts/Audio/SFXConstants.cs
+++ Assets/Scripts/Audio/SFXConstants.cs
@@ -2,18 +2,40 @@ namespace Audio
 {
     public static class SFXConstants
     {
+        //BG Tracks
+        public static string PostRound = "post_round";
+        public static string LaterLevel = "later_level";
+        public static string Battle1 = "battle_1";
+        public static string BossSouvlaki = "boss_souvlaki";
+
+        //Ambiance
+        public static string MainAtmos = "main_atmos";
+
+        //SFX
+        public static string Click = "click";
+        public static string Type = "type";
+        //Player Actions
         public static string PlayerAttack = "attack";
-        public static string PlayerAttackCharge = "charge";
-        public static string Hit = "hit";
         public static string Jump = "jump";
-        public static string Pickup = "pickup";
-        public static string Drop = "drop";
-        public static string Throw = "throw";
         public static string Help = "help";
+        public static string Hit = "hit";
+        //Clock
+        public static string Clock = "clock";
+        //Item Objective
         public static string ItemCollect = "item_collect";
         public static string ItemReject = "item_reject";
-        public static string Clock = "clock";
         public static string ObjectiveComplete = "objective_complete";
+        //Item Player
+        public static string PlayerAttackCharge = "charge";
+        public static string Drop = "drop";
+        public static string Throw = "throw";
+        public static string Pickup = "pickup";
+        //Enemies
+        public static string BossSouvlakiExplosion = "boss_souvlaki_explosion";
         public static string SmallEnemyHit = "small_enemy_hit";
+        public static string BossSummon = "boss_summon";
+        public static string ExplosionBlock = "explosion_block";
+        public static string SmallEnemyAttack = "small_enemy_attack";
+        public static string BurgerAttack = "burger_attack";
     }
 }
